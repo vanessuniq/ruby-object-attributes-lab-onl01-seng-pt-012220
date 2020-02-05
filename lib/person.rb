4bcaf834744320453a5e82f1bf83b4728cd8cebb
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-05 04:56:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,4 +6,17 @@ class Person
   def name= (name)
     @name = name
   end
+  
+  def name
+    @name
+  end
+  
+  def job= (my_job)
+    @job = my_job
+  end
+  
+  def job
+    @job
+  end
+  
 end
