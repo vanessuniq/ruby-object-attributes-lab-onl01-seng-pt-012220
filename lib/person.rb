7e8a0763e7c74a7d17984c05eec5789244fed447
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-05 04:59:10 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name= (name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job= (my_job)
+    @job = my_job
+  end
+  
+  def job
+    @job
+  end
+  
+end
